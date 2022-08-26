b4513e135f74b5e9322551ad1d2a2ea37da4086b
Thomas Basham <Bashamtg@gmail.com>
2022-08-25 19:04:48 -0700

    cleaned up db initializer

--- db.js
+++ db.js
@@ -1,6 +1,35 @@
+require("dotenv").config();
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccountKey.json");
+// var serviceAccount = require("./serviceAccountKey.json");
+
+
+const {
+  PROJECT_ID,
+  PRIVATE_KEY_ID,
+  PRIVATE_KEY,
+  CLIENT_EMAIL,
+  CLIENT_ID,
+  AUTH_URI,
+  TOKEN_URI,
+  AUTH_PROVIDER_X509_CERT_URL,
+  CLIENT_X509_CERT_URL
+
+} = process.env;
+
+var serviceAccount = 
+{
+  "type": "service_account",
+  "project_id": PROJECT_ID,
+  "private_key_id": PRIVATE_KEY_ID,
+  "private_key": PRIVATE_KEY,
+  "client_email": CLIENT_EMAIL,
+  "client_id": CLIENT_ID,
+  "auth_uri": AUTH_URI,
+  "token_uri": TOKEN_URI,
+  "auth_provider_x509_cert_url": AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": CLIENT_X509_CERT_URL
+}
 
 module.exports = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
