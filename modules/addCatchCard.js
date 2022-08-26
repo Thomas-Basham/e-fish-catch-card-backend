beba10d39ece1c1197000562f0a7654604dcffac
Thomas Basham <Bashamtg@gmail.com>
2022-08-25 17:56:39 -0700

    cleaned up

--- modules/addCatchCard.js
+++ modules/addCatchCard.js
@@ -1,5 +1,4 @@
 const firebase = require("../db");
-const CatchCard = require("../models/catchCard");
 const fireStore = firebase.firestore();
 
 const addCatchCard = async (req, res, next) => {
--- seed.js
+++ seed.js
@@ -1,12 +1,11 @@
 const firebase = require("./db");
 const db = firebase.firestore();
 
-
 const docRef = db.collection("catch-cards").doc("carp-card");
 
 async function postCatchCardData() {
   await docRef.set({
-    date_caught: Date.parse('05 Aug 2022 00:00:00 GMT'),
+    date_caught: Date.parse("05 Aug 2022 00:00:00 GMT"),
     species: "carp",
     weight: 18,
   });
