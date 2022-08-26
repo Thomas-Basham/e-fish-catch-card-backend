7cc29ee2e6b78c0e1886038dc4dc9669c31f00a3
Thomas Basham <Bashamtg@gmail.com>
2022-08-25 18:48:10 -0700

    cleaned up post route

--- modules/addCatchCard.js
+++ modules/addCatchCard.js
@@ -1,12 +1,16 @@
 const firebase = require("../db");
 const fireStore = firebase.firestore();
 
+const dbCollection = fireStore.collection("catch-cards").doc(Date.now().toString());
+
+
 const addCatchCard = async (req, res, next) => {
   try {
     console.log("Adding new Catch Card");
     const data = req.body;
-    await fireStore.collection("catch-cards").doc().set(data);
-    res.status(201).json({ message: "Record saved successfully" });
+    console.log(data)
+    await dbCollection.set(data);
+    res.status(200).json({ message: "Record saved successfully" });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- seed.js
+++ seed.js
@@ -1,13 +1,13 @@
 const firebase = require("./db");
 const db = firebase.firestore();
 
-const docRef = db.collection("catch-cards").doc("carp-card");
+const docRef = db.collection("catch-cards").doc("carp-card22222");
 
 async function postCatchCardData() {
   await docRef.set({
     date_caught: Date.parse("05 Aug 2022 00:00:00 GMT"),
-    species: "carp",
-    weight: 18,
+    species: "carps",
+    weight: 13,
   });
 }
 
