c35e7c93a17b007324429fe4f8b57d5b7702a8f9
Thomas Basham <Bashamtg@gmail.com>
2022-08-25 17:21:30 -0700

    post catch card route connected

--- postCatchCardData.js
+++ postCatchCardData.js
@@ -1,29 +1,14 @@
-const {
-  initializeApp,
-  applicationDefault,
-  cert,
-} = require("firebase-admin/app");
-const {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-} = require("firebase-admin/firestore");
+const firebase = require("./db");
+const db = firebase.firestore();
 
-initializeApp({
-  credential: applicationDefault(),
-});
 
-const db = getFirestore();
-
-const docRef = db.collection("catch-cards").doc("card");
+const docRef = db.collection("catch-cards").doc("carp-card");
 
 async function postCatchCardData() {
   await docRef.set({
-    date_caught: "2022-8-25",
-    body_of_water: "Lake Washington",
-    fish_length: 5,
-    species: "trout",
-    weight: 3,
+    date_caught: Date.parse('05 Aug 2022 00:00:00 GMT'),
+    species: "carp",
+    weight: 18,
   });
 }
 
